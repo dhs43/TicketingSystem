636084779e81eb1061909c0310741c75a0a86c9f
David Shapiro <davidshap3@gmail.com>
2019-10-13 23:50:59 +0000

    Kachow.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://bit.ly/35i7uFi" alt="kerchoo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span role="img" aria-label="lightning">⚡️</span> Kachow <span role="img" aria-label="lightning">⚡️</span>   
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
