ba18bfdcabe82773320a9491ab2908fb71f5123d
eal376 <42684154+eal376@users.noreply.github.com>
2019-10-13 17:32:06 -0700

    Added some routing and Home and Login files to the src

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import { Link, Route, HashRouter} from "react-router-dom";
+
+import Login from "./Login.jsx";
+import Home from "./Home.jsx";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://bit.ly/35i7uFi" alt="kerchoo" />
-        <p>
-          <span role="img" aria-label="lightning">⚡️</span> Kachow <span role="img" aria-label="lightning">⚡️</span>   
-        </p>
-      </header>
+     <HashRouter>
+         <div>
+             <Link to={"/Login"}>Login </Link>
+
+             <Route exact path={"/"} component={Home}></Route>
+             <Route exact path={"/Login"} component={Login}></Route>
+         </div>
+     </HashRouter>
     </div>
   );
 }
