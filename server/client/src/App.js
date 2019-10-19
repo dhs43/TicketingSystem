204f9be38a601aff74405ab05340b8d6f5dbb361
David Shapiro <davidshap3@gmail.com>
2019-10-19 14:41:30 -0700

    Express now correctly serves React app

---
+++ server/client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+import { Link, Route, HashRouter } from "react-router-dom";
+
+import Login from "./Login.jsx";
+import Home from "./Home.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <HashRouter>
+        <div>
+          <Link to={"/Login"}>Login </Link>
+
+          <Route exact path={"/"} component={Home}></Route>
+          <Route exact path={"/Login"} component={Login}></Route>
+        </div>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.get('/test', function (req, res) {
+app.get('/api/test', function (req, res) {
 	return res.send('Software Engineering - HSU Helpdesk');
 });
+
+if (process.env.NODE_ENV === 'production') {
+	console.log("In build mode");
+
+	// Express first tries to serve production assets
+	app.use(express.static(path.resolve('server/client/build/')));
+
+	// Express will serve index.html if it doesn't recognize route
+	app.get('*', (req, res) => {
+		console.log("Got here");
+		console.log(req.protocol + "://" + req.get('host') + req.originalUrl);
+		res.sendFile(path.resolve("server/client", "build", "index.html"));
+	});
+}
+
 
 app.listen(process.env.PORT || 8000);
 console.log("Server running on port " + PORT);
