b2761ceb799b495b849df1681c4fec9876cae89e
eal376 <42684154+eal376@users.noreply.github.com>
2019-10-29 16:53:23 -0700

    Revert "resolving conflicts"
    This reverts commit 668381f71daffb114e021d3051e4f7ed1802918e.

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,16 @@ import { Route, HashRouter } from "react-router-dom";
 
 import Home from "./Components/Home/Home.js";
 import Main from "./Components/Main/Main.js";
-// import SubmitTicket from "./Components/SubmitTicket/SubmitTicket";
+import SubmitTicket from "./Components/SubmitTicket/SubmitTicket";
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <div>
-<<<<<<< Updated upstream:client/src/App.js
           <Route exact path={"/"} component={Home}></Route>
           <Route exact path={"/Main"} component={Main}></Route>
           <Route exact path={"/SubmitTicket"} component={SubmitTicket}></Route>
-=======
-          <Route exact path={"/"} component={Main}></Route>
-          <Route exact path={"/Home"} component={Home}></Route>
-          {/*<Route exact path={"/SubmitTicket"} component={SubmitTicket}></Route>*/}
->>>>>>> Stashed changes:server/client/src/App.js
         </div>
       </HashRouter>
     </div>
