ea5f2477f6d83129ee6fe92ec5d8ef590142fc6a
dhs43 <davidshap3@gmail.com>
2019-11-10 17:04:07 -0800

    Converted table dates and added time-since value on comments

--- client/src/Components/Comment/Comment.js
+++ client/src/Components/Comment/Comment.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
+import TimeAgo from 'react-timeago'
 import "./Comment.css";
-
 
 class Comment extends Component {
     render() {
         return (
             <Paper>
                 <div className="box">
+                    <TimeAgo className="time-ago" date={this.props.creation_date} />
                     <h4>{this.props.author_ID}</h4>
                     <p>{this.props.text}</p>
                 </div>
