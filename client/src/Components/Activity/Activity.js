d410cf16ff383e8d01efe4508bf78afcf815ac8d
David Shapiro <davidshap3@gmail.com>
2019-11-23 21:53:56 -0800

    Viewing ticket marks it as read

--- client/src/Components/Activity/Activity.js
+++ client/src/Components/Activity/Activity.js
@@ -1,6 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ActivityComment from './ActivityComment';
 import './ActivityComment.css';
+
+export function MarkAsRead(ticket_ID, technician_ID) {
+    fetch('/activity/mark_as_read/' + ticket_ID + '/' + technician_ID, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.token }
+    })
+        .then(response => response.text())
+        .then(response => {
+            if (response === "Sucessfully updated last_seen_comment_ID") {
+                console.log('Success');
+            } else {
+                console.log(response);
+                console.log("Error marking ticket as read");
+            }
+        });
+}
+
 
 class Activity extends Component {
     // constructor(props) {
