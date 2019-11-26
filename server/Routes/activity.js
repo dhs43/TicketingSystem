f028ae73fb7177bc82cbd5d11aecc0569c7c92fe
David <davidshap3@gmail.com>
2019-11-26 15:31:31 -0800

    Fetching latest activity in progress...

--- server/Routes/activity.js
+++ server/Routes/activity.js
@@ -21,7 +21,6 @@ router.get('/mark_as_read/:ticket_id/:technician_id', (req, res, next) => {
                 res.send("Max comment error");
                 return null;
             } else {
-                console.log(result[0].comment_ID);
                 last_seen_comment_ID = result[0].comment_ID;
 
                 if (last_seen_comment_ID === undefined) {
@@ -62,29 +61,52 @@ router.get('/get_activity/:technician_id', (req, res, next) => {
     var technician_ID = req.params.technician_id;
     var updates = undefined;
 
+    // Fetch last comment_ID from all tickets
     var statement = 'SELECT ticket_ID, MAX(comment_ID) comment_ID\
                      FROM comments \
                      GROUP BY ticket_ID;';
 
     getConnection(function (err, connection) {
-        connection.query(statement, function (err, result) {
+        connection.query(statement, function (err, max_comment_result) {
             if (err) {
                 console.log(err);
                 res.send("Max comment error");
                 return null;
             } else {
-                for (var i = 0; i < result.length; i++) {
-                    console.log(result[i]);
-                }
-                res.send(result);
+                var new_comments = [];
+                var max_comment_result_length = max_comment_result.length;
+                max_comment_result.map((value, index) => {
+                    // Check if activity table shows technician has read it
+                    var read_statement = "SELECT last_seen_comment_ID \
+                                          FROM activity \
+                                          WHERE technician_ID = \"" + technician_ID + "\" \
+                                          AND ticket_ID = \"" + value.ticket_ID + "\";";
+
+                    connection.query(read_statement, function (err, last_seen_comment_result) {
+                        if (err) {
+                            console.log(err);
+                            res.send("Error fetching last-seen comments");
+                            return null;
+                        } else {
+                            if (last_seen_comment_result[0] !== undefined) {
+                                if (last_seen_comment_result[0].last_seen_comment_ID < value.comment_ID) {
+                                    new_comments.push(value.ticket_ID);
+                                }
+                            }
+                            if (index + 1 === max_comment_result_length) {
+                                res.send(new_comments);
+                            }     
+                        }
+                    });
+                });
             }
         });
         connection.release();
     });
 
     // 1. Check which tickets have out-of-date last-read comments
-    // A. Fetch last comment_ID from all tickets
-    // B. Check if activity table shows technician has read it
+        // A. Fetch last comment_ID from all tickets
+        // B. Check if activity table shows technician has read it
     // 1. If not, add it to an activity list.
     // 2. If read, skip it
     // 2. Fetch the most recent comments
